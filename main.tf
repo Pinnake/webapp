056e2b232be5b10f210f6431a7d09861799d1bd4
Pinnake <168257020+Pinnake@users.noreply.github.com>
2024-09-23 20:46:12 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,39 @@
-# Generate a unique ID for the instance
-resource "random_id" "instance_id" {
-  byte_length = 8
+variable "company" {
+  description = "The name of the company"
+  type        = string
 }
 
-# Template file for metadata startup script
-data "template_file" "linux-metadata" {
-  template = file("path/to/your/startup-script.sh")  # Adjust the path to your script
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
 }
 
-# Define the VPC network
-resource "google_compute_network" "vpc" {
-  name = "${var.company}-${var.app_name}-vpc"
-  auto_create_subnetworks = false
+variable "environment" {
+  description = "The environment (dev/prod/etc.)"
+  type        = string
 }
 
-# Define the subnet
-resource "google_compute_subnetwork" "network_subnet" {
-  name          = "${var.company}-${var.app_name}-subnet"
-  region        = var.gcp_region  # Make sure you have a variable for the region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.0.0.0/24"  # Adjust CIDR as necessary
+variable "gcp_region" {
+  description = "GCP region"
+  type        = string
 }
 
-# Define the Google Compute Instance
-resource "google_compute_instance" "vm_instance_public" {
-  name         = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}"
-  machine_type = var.linux_instance_type
-  zone         = var.gcp_zone
-  hostname     = "${var.app_name}-vm${random_id.instance_id.hex}.${var.app_domain}"
-  tags         = ["ssh", "http"]
+variable "gcp_zone" {
+  description = "GCP zone"
+  type        = string
+}
 
-  boot_disk {
-    initialize_params {
-      image = var.ubuntu_2004_sku
-    }
-  }
+variable "linux_instance_type" {
+  description = "Linux instance type"
+  type        = string
+}
 
-  metadata_startup_script = data.template_file.linux-metadata.rendered
+variable "ubuntu_2004_sku" {
+  description = "Ubuntu 20.04 image SKU"
+  type        = string
+}
 
-  network_interface {
-    network       = google_compute_network.vpc.name
-    subnetwork    = google_compute_subnetwork.network_subnet.name
-    access_config {}
-  }
+variable "app_domain" {
+  description = "Application domain"
+  type        = string
 }
