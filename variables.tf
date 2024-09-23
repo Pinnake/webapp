a72876ad59881fd9511a18ed8d158ce2bd549fc5
shinsoncjohnson1997@gmail.com <shinsoncjohnson1997@instance-jenkins.us-central1-f.c.bamboo-striker-431709-q3.internal>
2024-09-23 03:45:18 +0000

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "company" {
+    description = "The name of the company"
+    type        = string
+}
+
+variable "app_name" {
+    description = "The name of the application"
+    type        = string
+}
